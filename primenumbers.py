1d048817c8bf4c8cafda0868e52cf6f05961360a
Deepesh Gidwani <deepesh.gidwani@gmail.com>
2020-04-02 16:56:07 +0530

    Adding Comments

--- primenumbers.py
+++ primenumbers.py
@@ -7,8 +7,8 @@ flag = 0
 if(ntc>1):
     for i in range(2,int(ntc/2)):
         if ntc%i == 0:
-            flag = 1
-            break
+            flag = 1 #Setting Flag = 1 
+            break #Executing break statement
 
     if flag==1:
         print("Number is not a prime number")
