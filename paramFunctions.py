fedc29df6fcbb1d42208857cb58d1c12ff250d57
Deepesh Gidwani <deepesh@promied.com>
2020-04-09 13:30:19 +0530

    Functions

---
+++ paramFunctions.py
@@ -0,0 +1,23 @@
+#Function With Parameters
+'''
+def <functionName>(<parameters>):
+    stat 1
+    stat 2
+    ...
+    stat n
+'''
+
+def calcCommision(lsales,nsales,isales):
+    sales = lsales + nsales + isales
+    comm = sales*.05
+    role = 'Average'
+    if sales>=10000:
+        role = 'Star Performer'
+    return [comm,role]
+
+x = calcCommision(5000,2000,3000)
+print(x)
+y = calcCommision(50000,0,0)
+print(y)
+
+print(calcCommision(8000,200,0))
