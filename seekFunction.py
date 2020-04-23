122b6c7a5c9a92f7111c960eadfdd1f73ab3a02e
saranshsethi1995 <saransh1995@gmail.com>
2020-04-23 16:40:20 +0530

    Comments added

--- seekFunction.py
+++ seekFunction.py
@@ -4,16 +4,16 @@ fp.seek(10) #(10,0)
 fp.seek(10,1) #Current Position
 fp.seek(-10,2) #From End of File
 '''
-fp = open('filePractice.txt','rb')
+with open ('.\Assignment\Assignment.txt','rb') as fp:
 
-fp.seek(2)
-x = fp.read()
-print(x)
+    fp.seek(2)
+    x = fp.read(10)
+    print(x)
 
-fp.seek(-5,1)
-x = fp.read()
-print(x)
+    fp.seek(-5,1)
+    x = fp.read(10)
+    print(x)
 
-fp.seek(-5,2)
-x = fp.read()
-print(x)
+    fp.seek(-5,2)
+    x = fp.read(15)
+    print(x)
