0470ef0d9c6d14f1d1fed6b3f3dcdaa02ad5cf6e
Deepesh Gidwani <deepesh@promied.com>
2020-05-08 13:07:56 +0530

    Class Methods, Multiple Inheritence

--- SortFiles.py
+++ SortFiles.py
@@ -1,4 +1,7 @@
 # Sort Files
+# First: Should work for large files as well.
+# Second: Should work for inner directories as well. os.walk()
+
 import os
 extmap = {
     'txt':'text',
@@ -18,12 +21,13 @@ except:
 
 lstdir = os.listdir()
 
+
 for v in lstdir:
     if(os.path.isfile(v)):
         ext = v.split('.')[-1]
         directory = 'others'
         if ext in extmap.keys():
-            directory = extmap[ext]
+            directory = extmap[ext] #directory=pdf
         fp = open(v,'rb')
         filedata = fp.read()
         fpd = open(directory + '/' + v,'wb')
